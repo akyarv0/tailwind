a5c49941619d4e87b40337f58f0083ade525178e
akyarv0 <akyarv0@gmail.com>
2024-04-21 00:01:52 +0300

    colors

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import Typografy from './components/Typografy';
 
 function App() {
   return (
     <div className="App">
   <h1 className="bg-red-500 text-center ">Hello World</h1>
+  <Typografy/>
   
     </div>
   );
---
+++ src/components/Typografy.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Typografy = () => {
+  return (
+    <div ><p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p> </div>
+  )
+}
+
+export default Typografy
